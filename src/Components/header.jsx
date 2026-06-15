270b1356bf4d92a6f4de02fca379133fbc39c44b
Servet-Demir <demirservet586@gmail.com>
2026-06-15 10:16:18 +0300

    docs: delete unused documents

---
+++ src/Components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/Header.css';
+
+export function Header() {
+    return (
+        <div className='header'>
+            <div className='headerTitle'>Personel Takip Sistemi</div>
+        </div>
+    )
+}
+
+export default Header
