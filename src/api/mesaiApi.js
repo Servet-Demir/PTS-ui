f5bdbdb5e796c61dcc4e2046a59def397edb5fe4
Servet-Demir <demirservet586@gmail.com>
2026-06-22 12:42:46 +0300

    feat: add mesaiApi.js

---
+++ src/api/mesaiApi.js
@@ -0,0 +1,25 @@
+import api from "./axiosInstance";
+
+export const getAllMesailer = () => {
+    return api.get("/rest/api/mesai/list");
+};
+
+export const getMesaiByPersonelAndDonem = (personelId, donem) => {
+    return api.get(`/rest/api/mesai/personel/${personelId}`, {
+        params: {
+            donem: donem,
+        },
+    });
+};
+
+export const saveMesai = (mesai) => {
+    return api.post("/rest/api/mesai/save", mesai);
+};
+
+export const updateMesai = (id, mesai) => {
+    return api.put(`/rest/api/mesai/update/${id}`, mesai);
+};
+
+export const deleteMesai = (id) => {
+    return api.delete(`/rest/api/mesai/delete/${id}`);
+};
