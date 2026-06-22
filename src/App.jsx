9db679258d61b35bb056e06c1107fe7bd663335e
Servet-Demir <demirservet586@gmail.com>
2026-06-22 12:37:21 +0300

    feat: add routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
+import { Link, Route, Routes, Navigate } from "react-router-dom";
+
+import BirimPage from "./pages/BirimPage";
+import PersonelPage from "./pages/PersonelPage";
+import MesaiPage from "./pages/MesaiPage";
+import MaasPage from "./pages/MaasPage";
 
 function App() {
   return (
-    <Routes>
-    </Routes>
+    <div>
+      <h1>Personel Takip Sistemi</h1>
+
+      <nav>
+        <Link to="/birim">Birim</Link> |{" "}
+        <Link to="/personel">Personel</Link> |{" "}
+        <Link to="/mesai">Mesai</Link> |{" "}
+        <Link to="/maas">Maaş</Link>
+      </nav>
+
+      <hr />
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/birim" />} />
+        <Route path="/birim" element={<BirimPage />} />
+        <Route path="/personel" element={<PersonelPage />} />
+        <Route path="/mesai" element={<MesaiPage />} />
+        <Route path="/maas" element={<MaasPage />} />
+      </Routes>
+    </div>
   );
 }
 
