bf5a95b86bc276bacca09fe778fd8eddb6fad4f2
Servet-Demir <demirservet586@gmail.com>
2026-06-22 12:42:20 +0300

    feat: add personelApi.js

---
+++ src/api/personelApi.js
@@ -0,0 +1,17 @@
+import api from "./axiosInstance";
+
+export const getAllPersoneller = () => {
+    return api.get("/rest/api/personel/list");
+};
+
+export const savePersonel = (personel) => {
+    return api.post("/rest/api/personel/save", personel);
+};
+
+export const updatePersonel = (id, personel) => {
+    return api.put(`/rest/api/personel/update/${id}`, personel);
+};
+
+export const deletePersonel = (id) => {
+    return api.delete(`/rest/api/personel/delete/${id}`);
+};
