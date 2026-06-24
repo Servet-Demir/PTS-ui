454b0e10392ab1ecbaf8e5a4de19db5d60ab5171
Servet-Demir <demirservet586@gmail.com>
2026-06-24 10:40:05 +0300

    feat: add Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div>Navbar</div>
-    )
+        <header className="navbar">
+            <h1 className="navbar-title">Personel Takip Sistemi</h1>
+
+            <nav className="navbar-links">
+                <NavLink to="/birim">Birim</NavLink>
+                <NavLink to="/personel">Personel</NavLink>
+                <NavLink to="/mesai">Mesai</NavLink>
+                <NavLink to="/maas">Maaş</NavLink>
+            </nav>
+        </header>
+    );
 }
 
-export default Navbar
+export default Navbar;
