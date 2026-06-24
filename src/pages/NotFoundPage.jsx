8c97a7fa824c9916bbd01917a63145f3f9970573
Servet-Demir <demirservet586@gmail.com>
2026-06-24 09:47:03 +0300

    feat: add NotFoundPage.jsx

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+    return (
+        <div>
+            <h2>Sayfa Bulunamadı</h2>
+            <p>Aradığınız sayfa mevcut değil.</p>
+        </div>
+    );
+}
+
+export default NotFoundPage;
